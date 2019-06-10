2ff524e6df1fc3b5c0f7fe984b4bc7ae5086541b
Morgan-Mischo <goramorgan@gmail.com>
2019-06-10 15:54:02 -0600

    part2 finished

---
+++ Part2/index.js
@@ -0,0 +1,14 @@
+console.log("The house always wins!")
+
+let idInput = document.getElementById("idInput"); 
+let colorInput = document.getElementById("colorInput"); 
+
+console.log(idInput); 
+console.log(colorInput); 
+
+function setCard()
+{
+    const card = document.getElementById(idInput.value); 
+    console.log(card); 
+    card.style.color = colorInput.value; 
+}
